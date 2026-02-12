b66ff12bf2438ae7c7666fe5588983d1d6331700
SOBANA DEVI <117429292+sobanadeviv@users.noreply.github.com>
2026-02-12 11:34:36 +0530

    Add solution: 1. Two Sum

---
+++ 1. Two Sum/1. Two Sum.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem: 1. Two Sum
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/two-sum/description/
+ * Language: cpp
+ * Date: 2026-02-12
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
