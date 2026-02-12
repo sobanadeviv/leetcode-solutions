48928337852c5bd29d18553bee5f133c7b47667d
SOBANA DEVI <117429292+sobanadeviv@users.noreply.github.com>
2026-02-12 11:37:25 +0530

    Add solution: 1. Two Sum

---
+++ 1. Two Sum/1. Two Sum.py
@@ -0,0 +1,11 @@
+#
+# Problem: 1. Two Sum
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/two-sum/
+# Language: python3
+# Date: 2026-02-12
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
